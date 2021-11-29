aadbb30449cf0bab70e8848133f7daf9492e000c
getsim <getsimal@gmail.com>
2021-11-29 20:27:15 +0200

    Added PostgreSQL env var

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -7,19 +9,12 @@ from recourses.user import UserRegister
 from recourses.item import Item, ItemList
 from recourses.store import Store, StoreList
 
-from db import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'george'
 api = Api(app)
-
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 
 jwt = JWT(app, authenticate, identify)
 
@@ -30,5 +25,5 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-    db.init_app(app)
+
     app.run(port=5000, debug=True)
